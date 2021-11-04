8a2abb220d94d46a9d9df7034a30f1dfb44ff3fa
AyeletKaylee <ayeletwasserman@carmel6000.amitnet.org>
2021-11-04 13:00:53 +0200

    :turtle:

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 const Card: React.FC<CardProps> = ({ yellowText, whiteText, backgroundImage, cardContainerClass }) => {
 	return (
-		<div className={clsx(cardContainerClass, 'card-container')} style={{ /* '--background-image' */backgroundImage: `url(${backgroundImage})` }}>
+		<div className={clsx(cardContainerClass, 'card-container')} style={{ backgroundImage: `url(${backgroundImage})` }}>
 			<div className="yellow-text">{yellowText}</div>
 			<div className="white-text">{whiteText}</div>
 
